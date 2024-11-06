a169e7a0660044e9fd4743b5bd7ddaac00e1c87f
johannes <mojohannes06@gmail.com>
2024-11-06 10:31:10 +0200

    Loadedr fixed

--- FrontEnd/src/components/Authentication/ProtectedRoute.jsx
+++ FrontEnd/src/components/Authentication/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react"; 
+import Loader from '../../pages/Loader'
 
 const ProtectedRoute = ({ children, adminOnly }) => {
   const { isSignedIn, isLoaded, user } = useUser(); 
 
   if (!isLoaded) {
-    return <div className="border-gray-600 border-t-black border-2 animate-spin"></div>; 
+    return <div className="bg-gray-900 min-h-screen w-full"><Loader /></div>; 
   }
 
   if (!isSignedIn) {
